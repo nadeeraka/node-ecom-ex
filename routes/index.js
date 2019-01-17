b70be950099681be90001543969ebc9b7f5f3e9c
nadeeraka <nimanthanadeeraka@outlook.com>
2019-01-17 18:36:55 +0530

    basic logic added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+//model
+const product = require("../models/product/product");
 /* GET home page. */
+
+//find products
+// const findProducts = async () => {
+//   let result = await product.find();
+//   if (result) {
+//     return result;
+//   }
+//   return false;
+// };
+// const data = findProducts();
+// if (data) {
+//   console.log(data);
+// } else {
+//   console.log("undefined");
+// }
+
 router.get("/", function(req, res, next) {
-  res.render("shop/index", { title: "Express" });
+  //find products
+  product.find().then(result => {
+    console.log("done");
+    if (result) {
+      res.render("shop/index", { title: "Express", products: result });
+    } else {
+      res.send("404 server error!");
+    }
+  });
 });
 
 module.exports = router;
